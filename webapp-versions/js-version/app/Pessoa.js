4bc2db86f789b450931e4aa4a11796f82b2bc0fe
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-01-21 18:08:44 -0200

    dragging dumb way

--- webapp-versions/js-version/app/Pessoa.js
+++
@@ -1,5 +0,0 @@
-export class Pessoa {
-  fala() {
-    alert('oi')
-  }
-}
--- webapp-versions/js-version/app/app.js
+++ webapp-versions/js-version/app/app.js
@@ -1,10 +1,68 @@
-import { Pessoa } from './Pessoa';
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function( event ) {
+
+}, false);
+
+document.addEventListener("dragstart", function( event ) {
+event.dataTransfer.setData('text/plain',null)
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = .5;
+
+    onDragHandle(event);
+}, false);
+
+document.addEventListener("dragend", function( event ) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function( event ) {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function( event ) {
+    // highlight potential drop target when the draggable element enters it
+    if ( event.target.className == "dropzone" ) {
+        event.target.style.background = "purple";
+    }
+
+}, false);
+
+document.addEventListener("dragleave", function( event ) {
+    // reset background of potential drop target when the draggable element leaves it
+    if ( event.target.className == "dropzone" ) {
+        event.target.style.background = "";
+    }
+
+}, false);
+
+document.addEventListener("drop", function( event ) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if ( event.target.className == "dropzone" ) {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild( dragged );
+        event.target.appendChild( dragged );
+    }
+
+    console.log(elDragPositionX)
+  
+}, false);
+
+
+
+
 
 let elDragPositionX;
 let elDragPositionY;
-
 function onDragHandle(event) {
-
   event.dataTransfer.setData('text/plain', event.target.id)
 
   const data = event.dataTransfer.getData('text');
@@ -16,7 +74,9 @@ function onDragHandle(event) {
 }
 
 
+
 function onDropHandle(event) {
+  
   event.preventDefault();
   const draggedElementId = event.dataTransfer.getData('text');
   const draggedElement = document.getElementById(draggedElementId);
@@ -30,4 +90,3 @@ function onDropHandle(event) {
   draggedElement.style.left = `${event.clientX - elDragPositionX}px`;
   draggedElement.style.top = `${event.clientY - elDragPositionY}px`;
 }
-
