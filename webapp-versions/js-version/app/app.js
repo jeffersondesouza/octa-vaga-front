59d6709dedcf019001798de80ac44a7cf02a439d
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-01-21 17:22:42 -0200

    main layput ok

--- webapp-versions/js-version/app/app.js
+++ webapp-versions/js-version/app/app.js
@@ -1,4 +1,33 @@
 import { Pessoa } from './Pessoa';
 
+let elDragPositionX;
+let elDragPositionY;
 
-alert('oi')
+function onDragHandle(event) {
+
+  event.dataTransfer.setData('text/plain', event.target.id)
+
+  const data = event.dataTransfer.getData('text');
+  const draggedElement = document.getElementById(data);
+
+  elDragPositionX = event.clientX - draggedElement.offsetLeft;
+  elDragPositionY = event.clientY - draggedElement.offsetTop;
+
+}
+
+
+function onDropHandle(event) {
+  event.preventDefault();
+  const draggedElementId = event.dataTransfer.getData('text');
+  const draggedElement = document.getElementById(draggedElementId);
+
+  if (event.target.id !== draggedElementId) {
+    event.target.appendChild(document.getElementById(draggedElementId));
+  }
+
+  draggedElement.style.position = 'absolute';
+
+  draggedElement.style.left = `${event.clientX - elDragPositionX}px`;
+  draggedElement.style.top = `${event.clientY - elDragPositionY}px`;
+}
+
