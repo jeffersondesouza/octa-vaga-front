d7ecb0cf7b8c43e22a08cd90ebd576924460f2e7
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-01-22 15:16:34 -0200

    input text form

--- webapp-versions/js-version/app/domain/edit-design-modal/EditDesignModal.js
+++ webapp-versions/js-version/app/domain/edit-design-modal/EditDesignModal.js
@@ -9,14 +9,41 @@ export class EditDesignModal {
     this.hide();
   }
 
+  editInputGroup() {
+
+  }
+
+  editCheckoboxGroup() {
+
+  }
+
+  editButtonGroup() {
+
+  }
+
+  listenStyleChanges(editElementModal, selectedElement) {
+
+      console.log(selectedElement.className.includes('input-label-group'));
+    
+
+
+    new DomManipulator().getElementById('js-background-color')
+      .addEventListener('input', (ev) => {
+        const color = ev.target.value;
+        
+        
+      });
+  }
+
   show(selectedElement) {
     this.editElementModal.style.visibility = 'visible';
-    this.editElementModal.style.top = (`${selectedElement.offsetTop*2}px`)
+    this.editElementModal.style.top = (`${selectedElement.offsetTop * 2}px`)
     this.selectedElement = selectedElement;
+    this.listenStyleChanges(this.editElementModal, selectedElement);
   }
 
   hide() {
-    
+
     this.editElementModal.style.visibility = 'hidden';
     this.editElementModal.style.top = (`${0}px`)
 
