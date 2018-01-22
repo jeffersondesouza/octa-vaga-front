801a943345d41db0a2014b2b674d24040e59fd2a
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-01-22 18:27:52 -0200

    layout ok

--- webapp-versions/angular-version/src/app/core/core.module.ts
+++ webapp-versions/angular-version/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent
   ],
   exports: [
     HeaderComponent
